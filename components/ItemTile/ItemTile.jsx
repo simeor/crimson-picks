b4cf4c3940c51cd2847d5d71d6b298c61996a0ad
simeo russo <simeo94@hotmail.com>
2023-01-23 21:40:22 +0000

    first push of POC

---
+++ components/ItemTile/ItemTile.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import ItemTileStyle from "./ItemTileStyle.module.css";
+import FadeIn from "react-fade-in";
+
+const ItemTile = ({ data, index }) => {
+  const {
+    type = "",
+    name = "",
+    author = "",
+    titleOne = "",
+    titleTwo = "",
+    titleThree = "",
+    titleFour = "",
+    link = "",
+  } = data;
+
+  return (
+    <a href={link} target="_blank" rel="noreferrer">
+      <div
+        className={`${ItemTileStyle[`tileContainer${type}`]} ${
+          ItemTileStyle.tileContainer
+        }`}
+      >
+        <div className={ItemTileStyle.tileGridRows}>
+          <div className={ItemTileStyle.tileGridColumns}>
+            <div>
+              <h3 className={ItemTileStyle.secondaryText}>{type}</h3>
+            </div>
+            <div>
+              <FadeIn delay={500 + index * 300} transitionDuration={1000}>
+                <h1>{author}</h1>
+              </FadeIn>
+            </div>
+          </div>
+          <div className={ItemTileStyle.tileGridColumns}>
+            <div></div>
+            <div>
+              <h2 className={ItemTileStyle.secondaryText}>{name}</h2>
+            </div>
+          </div>
+          <div className={ItemTileStyle.tileGridColumns}>
+            <div className={ItemTileStyle.flexEndRow}>
+              <div>
+                <h3>{titleOne}</h3>
+                <h3>{titleTwo}</h3>
+              </div>
+            </div>
+            <div>
+              <h3>{titleThree}</h3>
+              <h3>{titleFour}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </a>
+  );
+};
+
+export default ItemTile;
---
+++ pages/Types.ts
@@ -0,0 +1,14 @@
+export type FetchedData = {
+  data: TileData[];
+};
+
+export type TileData = {
+  type: string;
+  name: string;
+  author: string;
+  titleOne: string;
+  titleTwo: string;
+  titleThree: string;
+  titleFour: string;
+  link: string;
+};
